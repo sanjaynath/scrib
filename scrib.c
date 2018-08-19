40216ea08f508c168e0e0e397fe48021d65b4efe
sanjaynath <nsanjay725@gmail.com>
2018-08-19 16:45:41 +0530

    Raw IO,added welcome msg and tildas

--- scrib.c
+++ scrib.c
@@ -5,39 +5,74 @@
 #include <errno.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <sys/ioctl.h>
 #include <termios.h>
 #include <unistd.h>
 
+/****************************** defines ******************************/
+
+#define CTRL_KEY(k) ((k) & 0x1f)
+#define SCRIB_VERSION "0.0.1"
+
+
+
+
+
 /******************************* data *******************************/
 
-struct termios orig_termios; //to store original terminal attributes
+//to store the size of terminal
+struct editorConfig {
+
+	int screenrows;
+  	int screencols;
+  	struct termios orig_termios;  //to store original terminal attributes
+};
+//global struct to store size of terminal
+struct editorConfig E;
+
+//struct termios orig_termios;
+
+
+
+
+
+
+
+
+
 
 
 /******************************* terminal *******************************/
 
 //error handling - errno variable stores the error
 void die(const char *s) {
-  perror(s);//to print out decriptive error message with tag 's' passed on to the function die
-  exit(1);
+
+	//clear the screen on exit
+	write(STDOUT_FILENO, "\x1b[2J", 4);
+  	write(STDOUT_FILENO, "\x1b[H", 3);
+
+  	perror(s);//to print out decriptive error message with tag 's' passed on to the function die
+  	exit(1);
 }
 
 void disableRawMode() {
 	//restore original attributes of terminal on exit from text editor
 	//orig_termios was used to store the original 
 	//terminal attributes before enetering the program
-	if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios) == -1)
+	if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &E.orig_termios) == -1)
     	die("tcsetattr");
 
 }
 void enableRawMode() {
 
 	//get original attributes of terminal
-	if (tcgetattr(STDIN_FILENO, &orig_termios) == -1) 
+	if (tcgetattr(STDIN_FILENO, &E.orig_termios) == -1) 
 		die("tcgetattr");
 
 	//to call disableRawMode automatically at exit
 	atexit(disableRawMode);
-	struct termios raw = orig_termios; //save the original terminal attributes
+	struct termios raw = E.orig_termios; //save the original terminal attributes
 	
 	//modify input flag c_lflag using & and ~ bit operation
 	//IXON to disable ctrl-Q and ctrl-S
@@ -64,28 +99,205 @@ void enableRawMode() {
 		die("tcsetattr");
 }
 
+//read a character from terminal and return it
+char editorReadKey() {
+    int nread;
+    char c;
+    while ((nread = read(STDIN_FILENO, &c, 1)) != 1) {
+      if (nread == -1 && errno != EAGAIN) 
+      	die("read");
+    }
+    return c;
+}
+
+int getCursorPosition(int *rows, int *cols) {
+  char buf[32];
+  unsigned int i = 0;
+
+  if (write(STDOUT_FILENO, "\x1b[6n", 4) != 4) return -1;
+
+  while (i < sizeof(buf) - 1) {
+    if (read(STDIN_FILENO, &buf[i], 1) != 1) break;
+    if (buf[i] == 'R') break;
+    i++;
+  }
+  buf[i] = '\0';
+  if (buf[0] != '\x1b' || buf[1] != '[') return -1;
+  if (sscanf(&buf[2], "%d;%d", rows, cols) != 2) return -1;
+  return 0;
+}
+
+//get size of terminal, stores the rows and coloumn size of terminal in 'ws'
+int getWindowSize(int *rows, int *cols) {
+    struct winsize ws;
+    if (ioctl(STDOUT_FILENO, TIOCGWINSZ, &ws) == -1 || ws.ws_col == 0) {
+        if (write(STDOUT_FILENO, "\x1b[999C\x1b[999B", 12) != 12) 
+        	return -1;
+      	editorReadKey();
+      	return getCursorPosition(rows, cols);
+    } else {
+      *cols = ws.ws_col;
+      *rows = ws.ws_row;
+      return 0;
+    }
+}
+
+
+
+
+
+
+
+
+
+
+/*********************** append buffer **********************/
+
+//to create our own dynamic string which suports append operation, 
+//since does not support dynamic strings
+struct abuf {
+  char *b;
+  int len;
+};
+#define ABUF_INIT {NULL, 0}
+
+
+//append operation on string
+void abAppend(struct abuf *ab, const char *s, int len) {
+    char *new = realloc(ab->b, ab->len + len);
+    if (new == NULL) return;
+    memcpy(&new[ab->len], s, len);
+    ab->b = new;
+    ab->len += len;
+}
+void abFree(struct abuf *ab) {
+    free(ab->b);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/******************************** output *************************/
+
+//Write a welcome message at 1/3 of the screen
+//draw tildas at the beginning of every row except welcome msg line
+//no of rows is obtained by getWindowSize and stored in E.screenrows
+void editorDrawRows(struct abuf *ab) {
+  int y;
+  for (y = 0; y < E.screenrows; y++) {
+
+  		//Display welcome message
+      	if (y == E.screenrows / 3) {
+      		char welcome[80];
+      		int welcomelen = snprintf(welcome, sizeof(welcome),
+      		  "SCRIB editor -- version %s", SCRIB_VERSION);
+      		
+      		//if terminal window size is not big enough to fit our msg, truncate
+      		if (welcomelen > E.screencols) 
+      			welcomelen = E.screencols;
+
+      		//centering the welcome msg
+      		int padding = (E.screencols - welcomelen) / 2;
+		    if (padding) {
+		      abAppend(ab, "~", 1);
+		      padding--;
+		    }
+		    while (padding--) 
+		    	abAppend(ab, " ", 1);
+
+      		abAppend(ab, welcome, welcomelen);
+    	} else {
+      		abAppend(ab, "~", 1);//draw tilda
+    	}
+    	abAppend(ab, "\x1b[K", 3);
+    	if (y < E.screenrows - 1) {
+      		abAppend(ab, "\r\n", 2);
+    	}
+  	}
+}
+
+//refresh screen
+void editorRefreshScreen() {
+    struct abuf ab = ABUF_INIT;
+
+    abAppend(&ab, "\x1b[?25l", 6);//hide the cursor
+  	//abAppend(&ab, "\x1b[2J", 4); //clear entire screen
+  	abAppend(&ab, "\x1b[H", 3);  //reposition cursor to top left corner
+
+  	editorDrawRows(&ab); //draw tildas
+  	abAppend(&ab, "\x1b[H", 3);
+  	abAppend(&ab, "\x1b[?25h", 6); //show the cursor
+
+  	write(STDOUT_FILENO, ab.b, ab.len);
+  	abFree(&ab);
+}
+
+
+
+
+
+
+
+
+
+
+/**************************** input *******************************/
+
+//read a key from terminal using editorReadKey() and handle it
+void editorProcessKeypress() {
+  char c = editorReadKey();
+  switch (c) {
+    case CTRL_KEY('q'): //quit when ctrl-q is pressed 
+      exit(0);
+      break;
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
 
 /******************************* init *******************************/
 
+void initEditor() {
+
+	//since it is passed by reference, 
+	//the values of E will be initialised with row and coloumn size of terminal
+    if (getWindowSize(&E.screenrows, &E.screencols) == -1) 
+  	    die("getWindowSize");
+}
+
+
 int main() {
+
 	enableRawMode();
-	char c;
-	
-	//to print out every character read
-	//while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q') {
+	initEditor();	
+
 	while (1) {
-    char c = '\0';
-    if (read(STDIN_FILENO, &c, 1) == -1 && errno != EAGAIN) 
-    	die("read");
-    if (iscntrl(c)) { 	//for control characters like ctrl-c, ctrl-a, etc.
-      printf("%d\r\n", c);
-    } else {
-      printf("%d ('%c')\r\n", c, c);
-    }
-    if (c == 'q') break;
+
+		editorRefreshScreen();
+    	editorProcessKeypress();
   }
 	return 0;
 }
